29f5c53d99eca4a2dbdb07de3e6fed6041c34213
Chris Kim <midaskim6969@yahoo.com>
2021-05-11 10:45:45 -0700

    completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,37 @@
+var orderHistory = [
+  {
+    title: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr.Axel',
+    orderNumber: '114-3941689-8772232',
+    price: '$34.00',
+    deliveredDate: '08/08/20'
+  },
+  {
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    orderNumber: '113-9984268-1280257',
+    price: '$41.33',
+    deliveredDate: '07/20/20'
+  },
+  {
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: 'unknown',
+    orderNumber: '114-2875557-9059409',
+    price: '$17.22',
+    deliveredDate: '07/07/20'
+  },
+  {
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    author: 'unknown',
+    orderNumber: '113-2883177-2648248',
+    price: '$94.95',
+    deliveredDate: '07/05/20'
+  },
+  {
+    title: 'The Art of Sql',
+    author: 'Faroult, Stephane',
+    orderNumber: '113-2883177-2648248',
+    price: '$33.99',
+    deliveredDate: '07/05/20'
+  }
+];
