4c7fcf6c52aea81de86c2c0faaa98b45919f5500
Chris Kim <midaskim6969@yahoo.com>
2021-05-17 10:46:42 -0700

    completed dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+var h1 = document.querySelector('h1');
+console.log(h1);
+console.dir(h1);
+
+var expElem = document.querySelector('#explanation');
+console.log(expElem);
+console.log('.dir(expElem)');
+console.dir(expElem);
+
+var hintElem = document.querySelector('.hint');
+console.log(hintElem);
+console.log('.dir(hintElem)');
+console.dir(hintElem);
+
+var allElem = document.querySelector('p');
+console.log(allElem);
+
+var allExampleLinkElements = document.querySelectorAll('.example-link');
+console.log(allExampleLinkElements);
