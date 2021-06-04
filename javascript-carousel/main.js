ea3463d4d5dd881356f7807f06a708c1d3f38b02
Chris Kim <midaskim6969@yahoo.com>
2021-06-03 17:36:07 -0700

    completed javascript-carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -8,38 +8,38 @@ var $next = $('.next');
 var $prev = $('.prev');
 var img = $('img');
 var imgInterval = setInterval(imgChange, 2000);
-var Count = 1;
+var count = 1;
 
 function verifyImg() {
-  if (Count === 1) {
+  if (count === 1) {
     img.className = 'img01';
     img.setAttribute('src', 'images/001.png');
     $circleOne.className = 'fas fa-circle cir circleOne ';
     $circleTwo.className = 'far fa-circle cir circleTwo';
     $circleFive.className = 'far fa-circle cir circleFive';
 
-  } else if (Count === 2) {
+  } else if (count === 2) {
     img.className = 'img02';
     img.setAttribute('src', 'images/004.png');
     $circleOne.className = 'far fa-circle cir circleOne';
     $circleTwo.className = 'fas fa-circle cir circleTwo';
     $circleThree.className = 'far fa-circle cir circleThree';
 
-  } else if (Count === 3) {
+  } else if (count === 3) {
     img.className = 'img03';
     img.setAttribute('src', 'images/007.png');
     $circleTwo.className = 'far fa-circle cir circleTwo';
     $circleThree.className = 'fas fa-circle cir circleThree';
     $circleFour.className = 'far fa-circle cir circleFour';
 
-  } else if (Count === 4) {
+  } else if (count === 4) {
     img.className = 'img04';
     img.setAttribute('src', 'images/025.png');
     $circleThree.className = 'far fa-circle cir circleThree';
     $circleFour.className = 'fas fa-circle cir circleFour';
     $circleFive.className = 'far fa-circle cir circleFive';
 
-  } else if (Count === 5) {
+  } else if (count === 5) {
     img.className = 'img05';
     img.setAttribute('src', 'images/039.png');
     $circleOne.className = 'far fa-circle cir circleOne';
@@ -50,26 +50,26 @@ function verifyImg() {
 }
 
 function imgChange(event) {
-  Count++;
+  count++;
   verifyCount();
   verifyImg();
 }
 
 function verifyCount() {
-  if (Count > 5) {
-    Count = 1;
+  if (count > 5) {
+    count = 1;
   }
 }
 
 function verifyCountFloor() {
-  if (Count <= 0) {
-    Count = 5;
+  if (count <= 0) {
+    count = 5;
   }
 }
 
 $next.addEventListener('click', function (event) {
   clearInterval(imgInterval);
-  Count--;
+  count--;
   verifyCountFloor();
   verifyImg();
   imgInterval = setInterval(imgChange, 2000);
@@ -77,7 +77,7 @@ $next.addEventListener('click', function (event) {
 
 $prev.addEventListener('click', function (event) {
   clearInterval(imgInterval);
-  Count++;
+  count++;
   verifyCount();
   verifyImg();
   imgInterval = setInterval(imgChange, 2000);
