236104b6c8e578f6632f727a82940233aa3e2828
Chris Kim <midaskim6969@yahoo.com>
2021-06-22 21:57:57 +0000

    completed node-module-system.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,19 @@
+console.log('typeof exports:', typeof exports);
+console.log('value of exports:', exports);
+// object
+
+console.log('typeof require:', typeof require);
+console.log('value of require:', require);
+// function
+
+console.log('typeof module:', typeof module);
+console.log('value of module:', module);
+// object
+
+console.log('typeof __filename:', typeof __filename);
+console.log('value of __filename:', __filename);
+// string
+
+console.log('typeof __dirname:', typeof __dirname);
+console.log('value of __dirname:', __dirname);
+// string
