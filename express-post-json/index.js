4dbca48575ce8d6cc57dc982fc5a2b981b08bfed
Chris Kim <midaskim6969@yahoo.com>
2021-06-30 22:21:26 +0000

    completed postgres-intro.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,26 +1,7 @@
 const express = ('express');
 const app = express;
 let nextId = 1;
-const grades = {
-  12: {
-    id: 12,
-    name: 'Tim Berners-Lee',
-    course: 'HTML',
-    score: 95
-  },
-  47: {
-    id: 47,
-    name: 'Brendan Eich',
-    course: 'JavaScript',
-    score: 100
-  },
-  273: {
-    id: 273,
-    name: 'Forbes Lindsay',
-    course: 'JavaScript',
-    score: 92
-  }
-};
+const grades = {};
 
 app.get('/api/grades', (req, res) => {
   const tArray = [];
