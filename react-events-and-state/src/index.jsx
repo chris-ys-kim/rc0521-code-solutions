560abb08162ab1957e99509dc379efcb0ceab689
Chris Kim <midaskim6969@yahoo.com>
2021-07-09 00:47:51 +0000

    completed react-events-and-state.

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (!this.state.isClicked) {
+      return <button onClick={this.handleClick}>Click Me!</button>;
+    } else {
+      return <button onClick={this.handleClick}>I`ve been Clicked!</button>;
+    }
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.getElementById('root')
+);
