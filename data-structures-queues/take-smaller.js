29406c3964edb497bc5a1c4bacea0e58eb1e7c4d
Chris Kim <midaskim6969@yahoo.com>
2021-08-04 08:16:11 +0000

    completed data-structures-queues.

---
+++ data-structures-queues/take-smaller.js
@@ -0,0 +1,22 @@
+/* exported takeSmaller */
+
+function takeSmaller(queue) {
+  if (queue.peek() === undefined) {
+    return undefined;
+  }
+  const first = queue.dequeue();
+  if (queue.peek() !== undefined) {
+    const second = queue.dequeue();
+    if (first < second) {
+      queue.enqueue(second);
+      return first;
+    } else if (first === second) {
+      queue.enqueue(first);
+      return first;
+    } else {
+      queue.enqueue(first);
+      return second;
+    }
+  }
+  return first;
+}
